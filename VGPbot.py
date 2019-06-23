480a4bc1e52390e0b30c3e41158110ecbba5b13e
fraulk <fratulker@gmail.com>
2019-06-23 23:54:45 +0200

    add: switch banner every day from user shots

--- VGPbot.py
+++ VGPbot.py
@@ -1,8 +1,9 @@
 import tweepy
-import time
-import datetime
+import time as t
+from datetime import datetime, time
 import json
 from random import randint
+import requests
 
 from secrets import *
 
@@ -15,12 +16,41 @@ def getId(tweet):
     id = json.dumps(tweet['id'])
     return id
 
-#Randomly retweet latest tweets
-def retweetRandmly(ids):
-    n = randint(0, len(ids) - 1)
-    print(ids[n])
-    api.retweet(ids[n])
-    print('Tweet retweeted at ', str(datetime.datetime.now()))
+# Randomly retweet latest tweets
+def retweetRandmly(tweets):
+    n = randint(0, len(tweets) - 1)
+    print(tweets[n])
+    api.retweet(tweets[n])
+    print('Tweet retweeted at', str(datetime.datetime.now()))
+
+# Need cleanup
+def editBanner(searchResult):
+    screenName = []
+    mediaUrl = []
+    for tweet in searchResult:
+        entities = json.dumps(tweet._json['entities'], sort_keys=True, indent=4)
+        entities = json.loads(entities)
+        screen_name = json.dumps(tweet._json['user']['screen_name'], sort_keys=True, indent=4)
+        screen_name = screen_name.replace('"', '')
+        if ('media' in entities):
+            media = entities['media']
+            screenName.append(screen_name)
+            mediaUrl.append(media[0]['media_url'])
+    n = randint(0, len(screenName) - 1)
+    print(mediaUrl[n], "by", screenName[n], "chosen as banner")
+    img_data = requests.get(mediaUrl[n]).content
+    with open('tmpbanner.jpg', 'wb') as handler:
+        handler.write(img_data)
+    api.update_profile_banner('tmpbanner.jpg')
+    api.update_profile(description = 'A bot who retweet #VGPUnite tweets (the most used hashtag in the community so). Created by @freaksboi Still in beta. Banner by @' + screenName[n])
+
+def is_time_between(begin_time, end_time, check_time=None):
+    # If check time is not given, default to current UTC time
+    check_time = check_time or datetime.utcnow().time()
+    if begin_time < end_time:
+        return check_time >= begin_time and check_time <= end_time
+    else: # crosses midnight
+        return check_time >= begin_time or check_time <= end_time
 
 if __name__ == '__main__':
     while True:
@@ -28,25 +58,15 @@ if __name__ == '__main__':
         r = api.search(q = '#VGPUnite filter:media -filter:retweets -filter:videos', count = '35', include_entities = 'true')
         # print(r)
         id = []
-        # file = open("log.txt", "w")
         for tweet in r:
             id.append(getId(tweet._json))
-        #     entities = json.dumps(tweet._json['entities'], sort_keys=True, indent=4)
-        #     entities = json.loads(entities)
-        #     screen_name = json.dumps(tweet._json['user']['screen_name'], sort_keys=True, indent=4)
-        #     file.write(json.dumps(tweet._json, sort_keys=True, indent=4))
-        #     print(entities)
-        #     if ('media' in entities):
-        #         media = entities['media']
-        #         file.write(json.dumps(media[0]['media_url'], sort_keys=True, indent=4))
-        #         file.write(json.dumps(media, sort_keys=True, indent=4))
-        #         print(screen_name)
-        #         print(media[0]['media_url'])
-        #     print(id)
-        # file.close()
+        
+        # New banner from shots every day
+        if(is_time_between(time(15, 00), time(16, 00))):
+            editBanner(r)
 
         # print(id)
         retweetRandmly(id)
 
         #Every hours
-        time.sleep(3600)
+        t.sleep(3600)
